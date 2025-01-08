7228a7869c7e9fe3d74fcb3f95879a5ee1a28030
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-01-08 21:57:21 +0100

    Initial commit 08.01.2025

---
+++ src/main/java/com/contaktnick/contaktnick/ContaktnickApplication.java
@@ -0,0 +1,13 @@
+package com.contaktnick.contaktnick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaktnickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaktnickApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/contaktnick/contaktnick/entity/User.java
@@ -0,0 +1,51 @@
+package com.contaktnick.contaktnick.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String nick;
+    @Column(nullable = false)
+    private String email;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Category> categories;
+
+    // Геттеры и сеттеры
+    public Long getId() {
+        return id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
+
+
+
