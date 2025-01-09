be42ed8036c7b505585b35850d553d8a3a426189
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-01-09 13:21:17 +0100

    Update 09.01.2025 - 13:20

---
+++ src/main/java/contaktnick/ContaktNickApplication.java
@@ -0,0 +1,13 @@
+package contaktnick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaktNickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaktNickApplication.class, args);
+	}
+
+}
