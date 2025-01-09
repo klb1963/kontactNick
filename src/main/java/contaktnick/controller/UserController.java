1dcf91ab16fdb3e60cdd9a0d5b87faf925fdf8b6
Leonid Kleimann <k_l_b_1963@hotmail.com>
2025-01-09 10:45:21 +0100

    Initial commit for dev branch

--- src/main/java/contaktnick/controller/UserController.java
+++ src/main/java/contaktnick/controller/UserController.java
@@ -1,7 +1,7 @@
-package com.contaktnick.contaktnick.controller;
+package contaktnick.controller;
 
-import com.contaktnick.contaktnick.entity.User;
-import com.contaktnick.contaktnick.repository.UserRepository;
+import contaktnick.entity.User;
+import contaktnick.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
